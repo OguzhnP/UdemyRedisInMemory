16fa3521ab984773ed760353015b2b103e1342b8
Oğuzhan Portakal <oguzhannportakal@gmail.com>
2025-09-21 17:56:31 +0300

    with json and byte

--- IDistributedCacheRedis.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedis.Web/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IDistributedCacheRedis.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
 
 namespace IDistributedCacheRedis.Web.Controllers
 {
@@ -12,7 +14,14 @@ namespace IDistributedCacheRedis.Web.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            await _distributedCache.SetStringAsync("name", "fatih", new DistributedCacheEntryOptions
+            Product product = new Product { Id = 1, Name = "Kalem", Price = 200 };
+            var jsonProduct = System.Text.Json.JsonSerializer.Serialize(product);
+
+            // set byte
+            //Byte[] byteProduct = Encoding.UTF8.GetBytes(jsonProduct);
+            //_distributedCache.Set($"Product:{product.Id}", byteProduct);
+
+            await _distributedCache.SetStringAsync($"Product:{product.Id}",jsonProduct, new DistributedCacheEntryOptions
             {
                 AbsoluteExpiration = DateTime.Now.AddSeconds(220)
             });
@@ -20,8 +29,13 @@ namespace IDistributedCacheRedis.Web.Controllers
         }
         public async Task<IActionResult> Show()
         {
-            var res = await _distributedCache.GetStringAsync("name");
-            ViewBag.name = res;
+            //get byte
+            //Byte[] byteProduct = _distributedCache.Get("Product:1");
+            //string jsonProduct = Encoding.UTF8.GetString(byteProduct);
+
+            var res = await _distributedCache.GetStringAsync("Product:1");
+            var product = System.Text.Json.JsonSerializer.Deserialize<Product>(res);
+            ViewBag.product = product;
             return View();
         }
         public async Task<IActionResult> Remove()
