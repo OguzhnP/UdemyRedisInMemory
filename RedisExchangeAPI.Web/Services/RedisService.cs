915d027dc69d33b1799ba67c452a5fbef7cb5673
Oğuzhan Portakal <oguzhannportakal@gmail.com>
2025-09-22 23:41:36 +0300

    RedisExchangeAPI

---
+++ RedisExchangeAPI.Web/Services/RedisService.cs
@@ -0,0 +1,31 @@
+﻿using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Services
+{
+    public class RedisService
+    {
+        private readonly string _redisHost;
+        private readonly string _redisPort;
+        public IDatabase db { get; set; }
+        private ConnectionMultiplexer _redis;
+        public RedisService(IConfiguration configuration)
+        {
+            _redisHost = configuration["Redis:Host"];
+            _redisPort = configuration["Redis:Port"];
+        }
+
+        public void Connect()
+        {
+            _redis = ConnectionMultiplexer.Connect($"{_redisHost}:{_redisPort}");  
+        }
+        public IDatabase GetDb(int db = -1)
+        {
+            if (_redis == null || !_redis.IsConnected)
+            {
+                Connect();
+            }
+            return _redis.GetDatabase(db);
+        }   
+
+    }
+}
