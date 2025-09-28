a85b4d4de096a3f7a4e503d90be2ead176d64d79
Oğuzhan Portakal <oguzhannportakal@gmail.com>
2025-09-28 04:30:45 +0300

    added redis set type codes

---
+++ RedisExchangeAPI.Web/Controllers/SetTypeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class SetTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase _database;
+        private readonly string listKey = "hashNames";
+        public SetTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            _database = _redisService.GetDb(0);
+        }
+
+        public IActionResult Index()
+        {
+            HashSet<string> nameList = new HashSet<string>();
+            if (_database.KeyExists(listKey))
+            {
+                _database.SetMembers(listKey).ToList().ForEach(c =>
+                {
+                    nameList.Add(c .ToString());
+                });
+            }
+              
+            return View(nameList);
+        }
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            if(!_database.KeyExists(listKey))
+                _database.KeyExpire(listKey, DateTime.Now.AddMinutes(10));
+
+            _database.SetAdd(listKey, name);
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> DeleteItem(string name)
+        {
+            await _database.SetRemoveAsync(listKey, name); 
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
